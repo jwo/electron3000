e6abe3ec6eac5a146df7553cd1e9e7be0d1f563e
Jesse Wolgamott <jesse@comalproductions.com>
2016-11-21 12:31:43 -0600

    Only validate uniqueness when casting ballot

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -15,14 +15,14 @@ class Ballot < ApplicationRecord
   belongs_to :second_place_dessert, class_name: 'Entry'
   belongs_to :third_place_dessert, class_name: 'Entry'
 
-  validate do
+  validate on: :update do
     len = [first_place_side, second_place_side, third_place_side].uniq.length
     if len != 3
       errors.add(:first_place_side, "It looks like some sides are voted for twice. No good!")
     end
   end
 
-  validate do
+  validate on: :update do
     len = [first_place_dessert, second_place_dessert, third_place_dessert].uniq.length
     if len != 3
       errors.add(:first_place_dessert, "It looks like some desserts are voted for twice. No good!")
